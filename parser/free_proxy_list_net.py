97be79d49fbd1b8785ae4da460f007e2d8f0fe64
matvejcekasov <chekashovmatvey@gmail.com>
2024-06-22 21:28:43 +0300

    feat: Добавлен новый User Agent

--- parser/free_proxy_list_net.py
+++ parser/free_proxy_list_net.py
@@ -18,6 +18,7 @@ user_agents = [
     'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
+    'Mozilla/5.0 (compatible; U; ABrowse 0.6; Syllable) AppleWebKit/420+ (KHTML, like Gecko)',
 ]
 
 
